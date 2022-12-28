04e3ac315874fbb42d0c8877447b6177c2bdb197
degoev <50908619+degoev@users.noreply.github.com>
2022-12-29 01:05:02 +0530

    init

---
+++ lib/index.ts
@@ -0,0 +1,42 @@
+import connectorsByName, {
+  getLibrary,
+  connectorLocalStorageKey,
+} from "./connectors";
+import { getContract } from "./utils";
+import {
+  signMessage,
+  signTypedMessage,
+  registerToken,
+  setupNetwork,
+} from "./wallet-actions";
+import { supportedChains, rpcUrlByChains, ChainIds } from "./configs/networks";
+import walletConfigs from "./configs/wallets";
+import hooks from "./hooks";
+import redux from "./redux";
+
+const web3 = {
+  connectors: {
+    connectorsByName,
+    getLibrary,
+    connectorLocalStorageKey,
+  },
+  utils: {
+    getContract,
+  },
+  walletActions: {
+    setupNetwork,
+    signMessage,
+    signTypedMessage,
+    registerToken,
+  },
+  configs: {
+    supportedChains,
+    rpcUrlByChains,
+    ChainIds,
+    wallets: walletConfigs,
+  },
+  hooks,
+  redux,
+};
+
+export default web3;
