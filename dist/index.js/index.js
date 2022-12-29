1aa518b8d18c0067e65fd2cc6d744d29acaebeac
degoev <50908619+degoev@users.noreply.github.com>
2022-12-29 21:22:13 +0530

    dist

---
+++ dist/index.js/index.js
@@ -0,0 +1,22 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var index = require('./index-eed441b0.js');
+require('events');
+require('http');
+require('https');
+require('url');
+require('stream');
+require('assert');
+require('tty');
+require('util');
+require('os');
+require('zlib');
+require('path');
+require('fs');
+require('react');
+require('process');
+
+
+
+exports["default"] = index.web3;
+//# sourceMappingURL=index.js.map
---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+/**
+ * For more info
+ * see https://blog.logrocket.com/the-complete-guide-to-publishing-a-react-package-to-npm/
+ */
+
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { babel } from "@rollup/plugin-babel";
+import pkg from "./package.json";
+
+const config = {
+  input: "lib/index.js",
+  output: [
+    {
+      dir: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  plugins: [
+    resolve(),
+    json(),
+    babel({
+      babelHelpers: "runtime",
+      skipPreflightCheck: true,
+      plugins: [],
+      presets: ["next/babel"],
+    }),
+    commonjs(),
+  ],
+  external: ["react", "rect-dom"],
+};
+
+export default config;
